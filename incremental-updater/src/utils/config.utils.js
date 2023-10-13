507d4e6b190070d7d10d8e6f9ab59f97e0a3b060
King-Hin Leung <kinghin.leung@commercetools.com>
2023-10-13 10:23:49 +0200

    Revert change in config.utils in incremental-updater

--- incremental-updater/src/utils/config.utils.js
+++ incremental-updater/src/utils/config.utils.js
@@ -16,7 +16,7 @@ export const readConfiguration = () => {
     projectKey: process.env.CTP_PROJECT_KEY,
     scope: process.env.CTP_SCOPE,
     region: process.env.CTP_REGION,
-    locale: 'en-US',
+    locale: process.env.THEGOODSTORE_LOCALE,
   };
 
   const validationErrors = getValidateMessages(envValidators, envVars);
